d8f7f1f2de79738ef1b2c547855539a7bb366633
yuliyang <645644717@qq.com>
2020-09-27 14:46:36 +0800

    Initial commit

---
+++ app/src/main/java/com/yly/dragcloseview/PreviewActivity.kt
@@ -0,0 +1,129 @@
+package com.yly.dragcloseview
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AbsListView.OnScrollListener.SCROLL_STATE_IDLE
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.github.chrisbanes.photoview.PhotoView
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.activity_preview.*
+import kotlinx.android.synthetic.main.item_preview.view.*
+
+
+class PreviewActivity : AppCompatActivity() {
+
+    private var currentScrollState: Int = SCROLL_STATE_IDLE
+
+    private val dragCloseHelper by lazy {
+        DragCloseHelper(this)
+    }
+
+    private val mAdapter by lazy {
+        PagerAdapter()
+    }
+
+    private val index by lazy {
+        intent.getIntExtra("index", 0)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+        supportPostponeEnterTransition()
+        dragCloseHelper.setDragCloseViews(
+            containerView,
+            previewViewPager
+        )
+        dragCloseHelper.setDragCloseListener(object : DragCloseHelper.DragCloseListener {
+            override fun isDragEnable(): Boolean {
+                val currentScaleView: PhotoView? = previewViewPager.findViewById(R.id.previewImage)
+                currentScaleView?.apply {
+                    return !isFinishing && currentScrollState == SCROLL_STATE_IDLE && (scale == 1F || displayRect.top >= 0)
+                }
+                return false
+            }
+
+            override fun dragStart() {
+            }
+
+            override fun dragging(percent: Float) {
+            }
+
+            override fun dragCancel() {
+            }
+
+            override fun dragClose(isShareElementMode: Boolean) {
+                onBackPressed()
+            }
+        })
+
+        previewViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == index) {
+                    supportStartPostponedEnterTransition()
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                currentScrollState = state
+                super.onPageScrollStateChanged(state)
+            }
+        })
+        mAdapter.setHasStableIds(true)
+        previewViewPager.adapter = mAdapter
+        previewViewPager.setCurrentItem(index, false)
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        return if (dragCloseHelper.handleEvent(event)) {
+            true
+        } else {
+            super.dispatchTouchEvent(event)
+        }
+    }
+
+    override fun finishAfterTransition() {
+        setResult(Activity.RESULT_OK, Intent().apply {
+            putExtra("currentIndex", previewViewPager.currentItem)
+        })
+        super.finishAfterTransition()
+    }
+}
+
+class PagerAdapter : RecyclerView.Adapter<PagerAdapter.PagerHolder>() {
+
+    private val imags = intArrayOf(R.drawable.test_a, R.drawable.test_b)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerHolder {
+        return PagerHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_preview,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return imags.size
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun onBindViewHolder(holder: PagerHolder, position: Int) {
+        holder.itemView.previewImage.setImageResource(imags[position])
+    }
+
+    class PagerHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+        LayoutContainer
+}
